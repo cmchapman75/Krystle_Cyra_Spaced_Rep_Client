0529ba8c0d765e413371aa97eb4cac27ee899c75
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-02-03 17:17:23 -0500

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,7 +31,7 @@ class Header extends Component {
   renderLoginLink() {
     return (
       <nav>
-        <Link to='/login'>Login</Link>
+        <Link to='/login'>Login </Link>
         {' '}
         <Link to='/register'>Sign up</Link>
       </nav>
@@ -43,7 +43,7 @@ class Header extends Component {
       <header>
         <h1>
           <Link to='/'>
-            Spaced repetition
+            Spaced Repetition
           </Link>
         </h1>
         {TokenService.hasAuthToken()
