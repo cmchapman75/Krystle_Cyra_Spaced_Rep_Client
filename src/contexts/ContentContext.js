bac02c442b9077e6e94507a5c1573736c6724816
Christina Chapman <cmchapman75@gmail.com>
2020-02-04 10:36:20 -0600

    Fleshing out context

---
+++ src/contexts/ContentContext.js
@@ -0,0 +1,60 @@
+import React from '.react';
+import config from '../config';
+import TokenService from '../services/token-service';
+
+const ContentContext = React.createContext({
+    error: null,
+    language: {},
+    words: [],
+})
+
+export default ContentContext;
+
+export class ContentProvider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: null,
+            language: {},
+            words: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setContext();
+    }
+
+    setContext = () => {
+        this.getLanguage().then((resData) => this.setState({
+          error: null,
+          language: resData.language,
+          words: resData.words  
+        }));
+    }
+    
+    getLanguage = () => {
+        return fetch(`${config.API_ENDPOINT}/language`, {
+            method: 'GET',
+            header: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${TokenService.getAuthToken()}`
+            }
+        })
+        .then(res => res.json())
+        .then(response => response);
+    }
+
+    render() {
+        const content = {
+            error: this.state.error,
+            language: this.state.language,
+            words: this.state.words
+        }
+        return (
+            <ContentContext.Provider value={content}>
+                {this.props.children}
+            </ContentContext.Provider>
+        )
+    }
+}
