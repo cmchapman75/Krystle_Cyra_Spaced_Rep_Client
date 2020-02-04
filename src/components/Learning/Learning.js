f395a1f94f1b5dbc8ad153e4619305360a603481
Christina Chapman <cmchapman75@gmail.com>
2020-02-04 14:29:18 -0600

    Updated Context and Learning

--- src/components/Learning/Learning.js
+++ src/components/Learning/Learning.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import Stats from '../Stats/Statistics';
+import React from 'react'
+import Stats from '../Stats/Statistics'
 import ContentContext from '../../contexts/ContentContext';
+import './Learning.css'
 
 class Learning extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+  
     }
 
-    static contextType = ContentContext;
+  }
 
-    render() {
-        console.log(this.context.language.total_score)
-        return(
-            <div>
-                Learn Here
-                <Stats score={this.context.language.total_score}/>
+  static contextType = ContentContext;
+
+  render() {
+    return(
+      <div>
+        <div className="infoArea">
+          <div className="infoHeader">
+            Kill me.
+          </div>
+
+          <div className="wordsHolder">
+            <div className="words">
+              <h2>Translate the word:</h2>
+              <span>{this.context.head.nextWord}</span>
             </div>
-        )
-    }
+
+            <div className="guess">
+              <form>
+                <label for="learn-guess-input">What's the translation for this word?</label>
+                <input id="learn-guess-input" type="text" required/>
+                <button type="submit">Submit your answer</button>
+              </form>
+            </div>
+
+        </div>
+
+
+        </div>
+
+        <Stats score={this.context.language ? this.context.language.total_score : 0}/>
+      </div>
+    )
+  }
 }
 
 export default Learning;
--- src/contexts/ContentContext.js
+++ src/contexts/ContentContext.js
@@ -1,60 +1,73 @@
-import React from '.react';
+import React from 'react';
 import config from '../config';
 import TokenService from '../services/token-service';
+import LangService from '../services/lang-api-service';
 
 const ContentContext = React.createContext({
-    error: null,
-    language: {},
-    words: [],
+  error: null,
+  language: {},
+  words: [],
 })
 
 export default ContentContext;
 
+
 export class ContentProvider extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            error: null,
-            language: {},
-            words: [],
-        }
-    }
+    this.state = {
+      error: null,
+      language: {
+        id: 0,
+        name: 'placeholder',
+        user_id: 0,
+        head: 1,
+        total_score: 0
+      },
+      words: [],
+      head: {}
+    } 
+  }
 
-    componentDidMount() {
-        this.setContext();
-    }
+  componentDidMount() {
+    this.setContext();
+  }
 
-    setContext = () => {
-        this.getLanguage().then((resData) => this.setState({
-          error: null,
-          language: resData.language,
-          words: resData.words  
-        }));
-    }
-    
-    getLanguage = () => {
-        return fetch(`${config.API_ENDPOINT}/language`, {
-            method: 'GET',
-            header: {
-                'content-type': 'application/json',
-                'authorization': `Bearer ${TokenService.getAuthToken()}`
-            }
-        })
-        .then(res => res.json())
-        .then(response => response);
-    }
+  setContext = () => {
+    this.getLanguage().then((resData) => this.setState({
+      error: null,
+      language: resData.language,
+      words: resData.words
+    }));
+    LangService.getHead().then((resData) => this.setState({
+      head: resData
+    }))
+  }  
 
-    render() {
-        const content = {
-            error: this.state.error,
-            language: this.state.language,
-            words: this.state.words
-        }
-        return (
-            <ContentContext.Provider value={content}>
-                {this.props.children}
-            </ContentContext.Provider>
-        )
+  getLanguage = () => {
+    return fetch(`${config.API_ENDPOINT}/language`, {
+      method: 'GET',
+      headers: {
+        'content-type':'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then(res => res.json())
+    .then(response => response);
+  }
+
+  render() {
+    const content = {
+      error: this.state.error,
+      language: this.state.language,
+      words: this.state.words,
+      head: this.state.head
     }
+    return (
+      <ContentContext.Provider value={content}>
+        {this.props.children}
+      </ContentContext.Provider>
+    )
+  }
 }
