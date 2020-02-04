ececc00cfaa9c3b569f6f8edf81ea4d395b59eae
Christina Chapman <cmchapman75@gmail.com>
2020-02-04 14:48:57 -0600

    Added to Dashboard route

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Dashboard from '../../components/Dashboard/Dashboard';
+import Stats from '../../components/Stats/Statistics';
 
 
 class DashboardRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <Dashboard content={this.props.content} user={this.props.user}/>
+        <Stats score={this.props.content.language.total_score}/>
       </section>
     );
   }
