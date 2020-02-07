dfd56fd9a35283a345eea98bb1d8ae13fa4bf808
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-02-07 12:18:42 -0500

    learningroute

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
-import Learning from '../../components/Learning/Learning';
+import Learning from '../../components/Learning/Learning'
 
 class LearningRoute extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+        currentWord: {},
+        language: {}
+    }
+  }
+
+  
   render() {
     return (
-      <section>
-       <h2>Translate the word:</h2>
-       <span>Testnextword</span>
-       <p>Your total score is: 999</p>
-      <form></form>
-      <main>You have answered this word correctly 222 times.
-      You have answered this word incorrectly 333 times.
-      </main>
-      <label>What's the translation for this word? </label>
+      <section className="viewport">
+          <Learning />
       </section>
-
     );
   }
 }
 
 export default LearningRoute
- 
