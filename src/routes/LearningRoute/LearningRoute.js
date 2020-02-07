46b75b16237c3b85f806b4bdb210a0f861da196f
Christina Chapman <cmchapman75@gmail.com>
2020-02-07 10:37:45 -0600

    Fixed typo in LearningRoute.js

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -5,7 +5,7 @@ class LearningRoute extends Component {
   render() {
     return (
       <section>
-       {/* <h2>Translate the word:</h2>
+       <h2>Translate the word:</h2>
        <span>Testnextword</span>
        <p>Your total score is: 999</p>
       <form></form>
