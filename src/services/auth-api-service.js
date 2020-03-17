67154c9e791d803eba619ac01f2cee90ddda3030
Christina Chapman <cmchapman75@gmail.com>
2020-03-17 17:04:36 -0500

    working on server issues

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -22,7 +22,7 @@ const AuthApiService = {
       headers: {
         'content-type': 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify( username, password ),
     })
       .then(res =>
         (!res.ok)
